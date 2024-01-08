e40bad03367bdcd3a7de265b257c994b22c9f2a9
Letícia Boza <devleticiaboza@gmail.com>
2024-01-07 22:08:54 -0300

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Nokia Tijolo: ");
+Smartphone nokia = new Nokia(numero: "1399999999", modelo:"Nokia 60", imei: "123456", memoria: 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp..");
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartphone Iphone 16: ");
+Smartphone iphone = new Iphone(numero: "13988888888", modelo:"Iphone 16", imei: "654321", memoria: 32);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram..");
